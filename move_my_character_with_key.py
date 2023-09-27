592020dce02b113e310fce34d2291c24740006bc
jysabina03 <jysabina03@tukorea.ac.kr>
2023-09-28 02:55:10 +0900

    add_stop, add_right
    대기, 오른쪽 걷기 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,13 +1,13 @@
 from pico2d import *
 
 TUK_WIDTH,TUK_HEICHT=1280,1024
-open_canvas()
+open_canvas(TUK_WIDTH,TUK_HEICHT)
 character = load_image('animation_sheet.png')
 tuk_ground = load_image('TUK_GROUND.png')
 
 def handle_events():
     global running
-    global dirx,diry
+    global dirx,diry,dir_see
     # fill here
 
     events = get_events()
@@ -26,8 +26,10 @@ def handle_events():
         elif event.type==SDL_KEYUP:
             if event.key==SDLK_RIGHT:
                 dirx -= 1
+                dir_see = 1
             elif event.key == SDLK_LEFT:
                 dirx += -1
+                dir_see = -1
             elif event.key==SDLK_UP:
                 diry-=1
             elif event.key==SDLK_DOWN:
@@ -40,12 +42,35 @@ def handle_events():
 running = True
 x = 800 // 2
 y=800//2
-frame_run,frame_up,frame_down=0,0,0
+frame_see,frame_run,frame_up,frame_down=0,0,0,0
 dirx,diry=0,0
+dir_see = 1
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2,TUK_HEICHT//2)
+
+    if dirx == 0 and diry == 0:
+        if dir_see == 1:
+            character.clip_draw(29*frame_see,848,29,30,x,y,100,100)
+        frame_see = (frame_see + 1) % 10
+    elif dirx>0:
+        if frame_run <= 6:
+            character.clip_draw(26 * frame_run, 701, 26, 30, x, y, 100, 100)
+        elif frame_run<=10:
+            character.clip_draw(26*6+(frame_run-7)*31,701,31,30,x,y,100,100)
+        elif frame_run>10:
+            character.clip_draw(26*6+31*4+26*(frame_run-11),701,26,30,x,y,100,100)
+        frame_run = (frame_run + 1) % 12
+    elif dirx<0:
+        if frame_run <= 6:
+            character.clip_composite_draw(26 * frame_run, 701, 26, 30, x, y, 100, 100)
+            character.clip_draw(26 * frame_run, 701, 26, 30, x, y, 100, 100)
+        elif frame_run <= 10:
+            character.clip_draw(26 * 6 + (frame_run - 7) * 31, 701, 31, 30, x, y, 100, 100)
+        elif frame_run > 10:
+            character.clip_draw(26 * 6 + 31 * 4 + 26 * (frame_run - 11), 701, 26, 30, x, y, 100, 100)
+        frame_run = (frame_run + 1) % 12
 
     # 그리기
 
@@ -53,7 +78,7 @@ while running:
     handle_events()
     x+=dirx*7
     y+=diry*7
-    delay(0.05)
+    delay(0.1)
 
 # fill here
 
